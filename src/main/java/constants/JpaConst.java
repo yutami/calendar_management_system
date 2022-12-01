8808ae57422b728ac88622aa247f9992c5e92b8a
miyagiyuta <miyagiyuta@localhost>
2022-12-02 01:01:39 +0900

    準備

---
+++ src/main/java/constants/JpaConst.java
@@ -0,0 +1,78 @@
+package constants;
+
+/**
+ * DB関連の項目値を定義するインターフェース
+ * ※インターフェイスに定義した変数は public static final 修飾子がついているとみなされる
+ */
+public interface JpaConst {
+
+    //persistence-unit名
+    String PERSISTENCE_UNIT_NAME = "calendar_management_system";
+
+    //データ取得件数の最大値
+    int ROW_PER_PAGE = 15; //1ページに表示するレコードの数
+
+    //従業員テーブル
+    String TABLE_EMP = "users"; //テーブル名
+    //従業員テーブルカラム
+    String EMP_COL_ID = "id"; //id
+    String EMP_COL_CODE = "code"; //ユーザー番号
+    String EMP_COL_NAME = "name"; //氏名
+    String EMP_COL_PASS = "password"; //パスワード
+    String EMP_COL_ADMIN_FLAG = "admin_flag"; //管理者権限
+    String EMP_COL_CREATED_AT = "created_at"; //登録日時
+    String EMP_COL_UPDATED_AT = "updated_at"; //更新日時
+    String EMP_COL_DELETE_FLAG = "delete_flag"; //削除フラグ
+
+    int ROLE_ADMIN = 1; //管理者権限ON(管理者)
+    int ROLE_GENERAL = 0; //管理者権限OFF(一般)
+    int EMP_DEL_TRUE = 1; //削除フラグON(削除済み)
+    int EMP_DEL_FALSE = 0; //削除フラグOFF(現役)
+
+    //予定テーブル
+    String TABLE_REP = "reports"; //テーブル名
+    //予定テーブルカラム
+    String REP_COL_ID = "id"; //id
+    String REP_COL_EMP = "user_id"; //予定を作成したユーザのid
+    String REP_COL_REP_DATE = "report_date"; //いつの予定かを示す日付
+    String REP_COL_TITLE = "title"; //予定のタイトル
+    String REP_COL_CONTENT = "content"; //予定の内容
+    String REP_COL_CREATED_AT = "created_at"; //登録日時
+    String REP_COL_UPDATED_AT = "updated_at"; //更新日時
+
+    //Entity名
+    String ENTITY_EMP = "user"; //ユーザー
+    String ENTITY_REP = "report"; //予定
+
+    //JPQL内パラメータ
+    String JPQL_PARM_CODE = "code"; //社員番号
+    String JPQL_PARM_PASSWORD = "password"; //パスワード
+    String JPQL_PARM_EMPLOYEE = "user"; //ユーザー
+
+    //NamedQueryの nameとquery
+    //全てのユーザーをidの降順に取得する
+    String Q_EMP_GET_ALL = ENTITY_EMP + ".getAll"; //name
+    String Q_EMP_GET_ALL_DEF = "SELECT e FROM Employee AS e ORDER BY e.id DESC"; //query
+    //全てのユーザーの件数を取得する
+    String Q_EMP_COUNT = ENTITY_EMP + ".count";
+    String Q_EMP_COUNT_DEF = "SELECT COUNT(e) FROM Employee AS e";
+    //ユーザー番号とハッシュ化済パスワードを条件に未削除のユーザーを取得する
+    String Q_EMP_GET_BY_CODE_AND_PASS = ENTITY_EMP + ".getByCodeAndPass";
+    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE e.deleteFlag = 0 AND e.code = :" + JPQL_PARM_CODE + " AND e.password = :" + JPQL_PARM_PASSWORD;
+    //指定したユーザーを保持するユーザーの件数を取得する
+    String Q_EMP_COUNT_REGISTERED_BY_CODE = ENTITY_EMP + ".countRegisteredByCode";
+    String Q_EMP_COUNT_REGISTERED_BY_CODE_DEF = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
+    //全ての予定をidの降順に取得する
+    String Q_REP_GET_ALL = ENTITY_REP + ".getAll";
+    String Q_REP_GET_ALL_DEF = "SELECT r FROM Report AS r ORDER BY r.id DESC";
+    //全ての予定の件数を取得する
+    String Q_REP_COUNT = ENTITY_REP + ".count";
+    String Q_REP_COUNT_DEF = "SELECT COUNT(r) FROM Report AS r";
+    //指定したユーザーが作成した予定を全件idの降順で取得する
+    String Q_REP_GET_ALL_MINE = ENTITY_REP + ".getAllMine";
+    String Q_REP_GET_ALL_MINE_DEF = "SELECT r FROM Report AS r WHERE r.employee = :" + JPQL_PARM_EMPLOYEE + " ORDER BY r.id DESC";
+    //指定したユーザーが作成した予定の件数を取得する
+    String Q_REP_COUNT_ALL_MINE = ENTITY_REP + ".countAllMine";
+    String Q_REP_COUNT_ALL_MINE_DEF = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :" + JPQL_PARM_EMPLOYEE;
+
+}
