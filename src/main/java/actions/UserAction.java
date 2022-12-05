5e9e799aab21775135c4d26a35a612343e732e3e
miyagiyuta <miyagiyuta@localhost>
2022-12-06 01:37:02 +0900

    ユーザーnewアクションの作成

--- src/main/java/actions/UserAction.java
+++ src/main/java/actions/UserAction.java
@@ -61,7 +61,21 @@ public class UserAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
+    }
+
+        /**
+         * 新規登録画面を表示する
+         * @throws ServletException
+         * @throws IOException
+         */
+        public void entryNew() throws ServletException, IOException {
+
+            putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+            putRequestScope(AttributeConst.USER, new  UserView()); //空の従業員インスタンス
+
+            //新規登録画面を表示
+            forward(ForwardConst.FW_EMP_NEW);
+        }
 
     }
 
-}
